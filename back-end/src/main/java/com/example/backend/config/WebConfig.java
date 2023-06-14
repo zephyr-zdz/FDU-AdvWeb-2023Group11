f93cc34e632a72ba73bcb5f02667494d43bcc544
zhoudazhi <dazhi527@hotmail.com>
2023-06-14 12:02:45 +0800

    cors

--- back-end/src/main/java/com/example/backend/config/WebConfig.java
+++ back-end/src/main/java/com/example/backend/config/WebConfig.java
@@ -4,5 +4,5 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer { // WebMvcConfigurerAdapter is deprecated
 }
